ce820e65f16da41d45b9f9eef22cc25146b6e99f
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-11-12 00:32:58 -0500

    test

--- src/uiMain/Test.py
+++ src/uiMain/Test.py
@@ -1,4 +1,7 @@
-from src.gestorAplicacion.administracion.Cocinero import Cocinero
+from src.gestorAplicacion.administracion.Cocinero import *
+from src.gestorAplicacion.administracion.Mesero import *
 
-pepe = Cocinero("David", 100, 50000, "Calvas")
-print(pepe.getOcupacion())
+cocinero = Cocinero("David", 100, 50000, "Calvas")
+print(cocinero.getOcupacion())
+mesero = Mesero("Camilo", 100, 2000)
+print(mesero.accion())
