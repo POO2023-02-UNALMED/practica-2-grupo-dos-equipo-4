fbda597e15d63eed70865cd47001923e6c668714
Juanfepx <juanfepx@gmail.com>
2023-11-19 01:36:05 -0500

    serializacion de instancias

--- src/baseDatos/Serializador.py
+++ src/baseDatos/Serializador.py
@@ -14,3 +14,32 @@ pedido_serializado = Serializador.serializar(pedido1)
 
 # Deserializar el pedido
 pedido_deserializado = Serializador.deserializar(pedido_serializado)
+# Serializar un objeto Gaseosas
+gaseosas_serializada = Serializador.serializar(sprite)
+
+# Deserializar el objeto Gaseosas
+gaseosas_deserializada = Serializador.deserializar(gaseosas_serializada)
+
+# Serializar un objeto Ingredientes
+ingredientes_serializado = Serializador.serializar(pan)
+
+# Deserializar el objeto Ingredientes
+ingredientes_deserializado = Serializador.deserializar(ingredientes_serializado)
+
+# Serializar un objeto Comida
+comida_serializada = Serializador.serializar(clasicaDeCarne)
+
+# Deserializar el objeto Comida
+comida_deserializada = Serializador.deserializar(comida_serializada)
+
+# Serializar un objeto Mesero
+mesero_serializado = Serializador.serializar(camilo)
+
+# Deserializar el objeto Mesero
+mesero_deserializado = Serializador.deserializar(mesero_serializado)
+
+# Serializar un objeto Cocinero
+cocinero_serializado = Serializador.serializar(david)
+
+# Deserializar el objeto Cocinero
+cocinero_deserializado = Serializador.deserializar(cocinero_serializado)
